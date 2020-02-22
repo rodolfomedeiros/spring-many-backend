99c6b4683fd4b4f190ed013fca03be3d96760157
Rodolfo Medeiros <sd.rodolfo.medeiros@gmail.com>
2020-02-22 13:33:32 -0300

    init new book

---
+++ new-book-casa-do-codigo/src/test/java/com/devroods/newbookcasadocodigo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devroods.newbookcasadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
